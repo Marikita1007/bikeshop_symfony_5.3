fd19c86e8c55ab58bd4f25292aaca6f6cc18d2fe
Marikita1007 <ishinomaki1007@gmail.com>
2022-12-17 00:36:22 +0100

    Added file path in .gitignore

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class FirstController
 {
+    /**
+     * @Route("/first")
+     */
     public function homepage(): Response
     {
         return new Response(
